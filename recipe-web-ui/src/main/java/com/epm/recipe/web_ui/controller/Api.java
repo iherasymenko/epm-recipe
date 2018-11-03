040d208762daf289795d4dd92cb2562d301ffb31
Kateryna Kanivets <kanivets.kateryna@gmail.com>
2018-11-03 16:31:55 +0200

    Small additional configuration

---
+++ recipe-web-ui/src/main/java/com/epm/recipe/web_ui/controller/Api.java
@@ -0,0 +1,11 @@
+package com.epm.recipe.web_ui.controller;
+
+public class Api {
+    public static final String ROOT = "api";
+
+    public class Recipes {
+        public static final String RECIPES = "recipes";
+        public static final String RECIPE = "recipes/{recipe_id}";
+        public static final String RECIPE_OF_THE_DAY = "recipes/recipe_of_the_day";
+    }
+}
--- recipe-web-ui/src/main/java/com/epm/recipe/web_ui/controller/RestRecipeController.java
+++ recipe-web-ui/src/main/java/com/epm/recipe/web_ui/controller/RestRecipeController.java
@@ -2,18 +2,16 @@ package com.epm.recipe.web_ui.controller;
 
 import com.epm.recipe.domain.Recipe;
 import com.epm.recipe.service.RecipeService;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Objects;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("api")
+@RequestMapping(Api.ROOT)
 public class RestRecipeController {
 
     private final RecipeService recipeService;
@@ -22,13 +20,13 @@ public class RestRecipeController {
         this.recipeService = Objects.requireNonNull(recipeService, "recipeService");
     }
 
-    @GetMapping("recipe_of_the_day")
+    @GetMapping(Api.Recipes.RECIPE_OF_THE_DAY)
     public Recipe recipeOfTheDay() {
         return recipeService.recipeOfTheDay();
     }
 
-    @GetMapping("recipe/{id}")
-    public Optional<Recipe> recipe(@PathVariable long id) {
+    @GetMapping(Api.Recipes.RECIPE)
+    public Optional<Recipe> recipe(@PathVariable(value = "recipe_id") long id) {
         return recipeService.byId(id);
     }
 
