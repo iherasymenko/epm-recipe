0b712f29330b6227bc810bbaea1211bcc51f1410
Ihor Herasymenko <ihor_herasymenko@epam.com>
2019-08-02 19:45:12 +0300

    Allow external configuration to be specified via a property file

--- recipe-aggregator/src/main/java/com/epm/recipe/aggregator/config/AggregatorConfiguration.java
+++ recipe-aggregator/src/main/java/com/epm/recipe/aggregator/config/AggregatorConfiguration.java
@@ -3,13 +3,17 @@ package com.epm.recipe.aggregator.config;
 import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.FileSystemResource;
 
 @Configuration
 public class AggregatorConfiguration {
 
     @Bean
     public static PropertyPlaceholderConfigurer propertyPlaceholderConfigurer() {
-        return new PropertyPlaceholderConfigurer();
+        PropertyPlaceholderConfigurer configurer = new PropertyPlaceholderConfigurer();
+        configurer.setLocation(new FileSystemResource("conf/application.properties"));
+        configurer.setIgnoreResourceNotFound(true);
+        return configurer;
     }
 
 }
--- recipe-aggregator/src/main/java/module-info.java
+++ recipe-aggregator/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module recipe.aggregator {
     requires java.sql;
     requires spring.beans;
+    requires spring.core;
     requires spring.context;
     requires spring.web;
     requires spring.webmvc;
