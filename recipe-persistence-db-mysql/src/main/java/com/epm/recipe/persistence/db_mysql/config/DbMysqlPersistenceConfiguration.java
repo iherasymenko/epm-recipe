855514b0c48cf36ddcf0d7f71fbc50a884e94e43
mpalii <maksympalii.dev@gmail.com>
2019-08-05 00:14:31 +0300

    change mysql spring configuration

--- recipe-persistence-db-mysql/src/main/java/com/epm/recipe/persistence/db_mysql/config/DbMysqlPersistenceConfiguration.java
+++ recipe-persistence-db-mysql/src/main/java/com/epm/recipe/persistence/db_mysql/config/DbMysqlPersistenceConfiguration.java
@@ -2,16 +2,15 @@ package com.epm.recipe.persistence.db_mysql.config;
 
 import com.epm.recipe.persistence.RecipeRepository;
 import com.epm.recipe.persistence.db_mysql.DbMysqlRecipeRepository;
+import com.mysql.cj.jdbc.MysqlDataSource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 
 import javax.sql.DataSource;
-import java.sql.Driver;
 
 @Configuration
 @PropertySource("classpath:mysql.properties")
@@ -19,8 +18,6 @@ public class DbMysqlPersistenceConfiguration {
 
     @Value("${db.url}")
     private String url;
-    @Value("${db.driver}")
-    private String driverClassName;
     @Value("${db.user}")
     private String username;
     @Value("${db.password}")
@@ -36,20 +33,13 @@ public class DbMysqlPersistenceConfiguration {
         return new DbMysqlRecipeRepository(jdbcTemplate());
     }
 
-    @SuppressWarnings("unchecked")
     @Bean
     public DataSource dataSource() {
-        try {
-            SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
-            Class<? extends Driver> driver = (Class<? extends Driver>) Class.forName(driverClassName);
-            dataSource.setDriverClass(driver);
-            dataSource.setUrl(url);
-            dataSource.setUsername(username);
-            dataSource.setPassword(password);
-            return dataSource;
-        } catch (ClassNotFoundException e) {
-            return null;
-        }
+        MysqlDataSource mysqlDataSource = new MysqlDataSource();
+        mysqlDataSource.setUrl(url);
+        mysqlDataSource.setUser(username);
+        mysqlDataSource.setPassword(password);
+        return mysqlDataSource;
     }
 
     @Bean
--- recipe-persistence-db-mysql/src/main/java/module-info.java
+++ recipe-persistence-db-mysql/src/main/java/module-info.java
@@ -5,7 +5,9 @@ module recipe.persistence.db_mysql {
     requires spring.beans;
     requires spring.jdbc;
     requires java.sql;
+    requires java.naming;
     requires slf4j.api;
+    requires mysql.connector.java;
     opens com.epm.recipe.persistence.db_mysql to spring.core;
     opens com.epm.recipe.persistence.db_mysql.config to spring.core;
     exports com.epm.recipe.persistence.db_mysql.config;
